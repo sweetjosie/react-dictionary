e9f176ae9de129b91a4706c4e13f0d5dfb02d9b1
Josie <sweetjosieme@gmail.com>
2022-10-09 20:10:10 -0700

    search engine set up

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
+      <div className='container'> 
       <header className="App-header">
       <h1>Dictionary</h1>
+      <main>
+      <Dictionary/>
+      </main>
       </header>
+      <footer className='text-center'> Open Source coded by Josie Maia</footer>
+    </div>
     </div>
   );
 }
