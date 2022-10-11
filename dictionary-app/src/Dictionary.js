96d66aa4f625922263dcc10d0975e9ef8046e475
Josie <sweetjosieme@gmail.com>
2022-10-10 22:30:52 -0700

    results component

--- dictionary-app/src/Dictionary.js
+++ dictionary-app/src/Dictionary.js
@@ -1,18 +1,21 @@
 import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results.js";
 
 export default function Dictionary(){
     let [keyword, setKeyword] = useState("")
+    let [results, setResults] = useState({});
 
     function handleResponse(response){
-        console.log(response.data[0]);
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
     }
 
     function search(event){
         event.preventDefault();
            
-
+        // source:https://dictionaryapi.dev/
          let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`   ;
          axios.get(apiUrl).then(handleResponse);
         }
@@ -31,6 +34,7 @@ export default function Dictionary(){
             onChange={storeKyeword}
             autoFocus={true}/>
         </form>
+        <Results results={results}/>
          </div>
     )  ;
 }
