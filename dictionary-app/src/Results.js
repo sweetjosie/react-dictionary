c1ea824a2ced47b27339c7b364f1e680e8a12a45
Josie <sweetjosieme@gmail.com>
2022-10-11 12:34:03 -0700

    Phonetics component

--- dictionary-app/src/Results.js
+++ dictionary-app/src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetics from "./Phonetics.js"
 
 
 
@@ -8,6 +9,14 @@ export default function Results(props){
 
         return ( <div className="Results">
             <h2>{props.results.word}</h2>
+            {props.results.phonetics.map(function(phonetic, index){
+                return (
+                    <div key={index}>
+                        <Phonetics phonetic = {phonetic}/>
+                        </div>
+                );
+            })}
+
             {props.results.meanings.map(function(meaning,index){
                 return (
                     <div key= {index}>
